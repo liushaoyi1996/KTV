f3afdac851371bf00494c398f7dac52a5553b775
liushaoyi <1065102050@qq.com>
2023-05-22 00:27:01 +0800

    日志

---
+++ src/main/java/com/KTV/controller/testController.java
@@ -0,0 +1,12 @@
+package com.KTV.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class testController {
+    @GetMapping("/test")
+    public void test(){
+        System.out.println("卧槽！！！！！");
+    }
+}
---
+++ src/main/java/com/KTV/ktvApplication.java
@@ -0,0 +1,13 @@
+package com.KTV;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ktvApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ktvApplication.class, args);
+    }
+
+}
